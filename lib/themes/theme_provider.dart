37d8a3f43e5ce99b2e789e91da54f19c58a655f5
eduardani84 <eduardani84@gmail.com>
2025-03-04 01:58:45 +0000

    Theme light and darkMode

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/themes/dark_mode.dart';
+import 'package:myapp/themes/light_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  void setThemeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      _themeData = darkMode;
+    } else {
+      _themeData = lightMode;
+    }
+  }
+}
